18607dd93a0ebcc4036eadb832045b92ef6dd75b
ElmerPC <erey2790@gmail.com>
2022-07-28 13:43:59 -0500

    persistance with idbpromise

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
+import { idbPromise } from '../../utils/helpers';
+
 
 const CartItem = ({ item }) => {
     const [, dispatch] = useStoreContext();
@@ -11,6 +13,7 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', { ...item})
     };
 
     const onChange = (e) => {
@@ -26,7 +29,8 @@ const CartItem = ({ item }) => {
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
-            })
+            });
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
         }
     }
     return (
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -28,6 +28,8 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+    // upon removal from cart, delete the item from IndexedDB using the 'currentProduct._id' to locate what to remove. 
+    idbPromise('cart', 'delete', { ...currentProduct })
   };
 
   useEffect(() => {
@@ -67,11 +69,19 @@ function Detail() {
       _id: id,
       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
     });
+
+  // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+  idbPromise('cart', 'put', {
+    ...itemInCart,
+    purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+  })
    } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...currentProduct, purchaseQuantity: 1 }
     });
+    // if product isnt in the cart yet, add it to the current shopping cart in IndexedDB
+    idbPromise('cart', 'put',  { ...currentProduct, purchaseQuantity: 1 })
    }
   };
 
