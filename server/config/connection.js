0ae7e73194bfd20758b361ce1426cc5b91ab4563
ElmerPC <erey2790@gmail.com>
2022-08-10 00:07:40 -0500

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SHOP-SHOP2', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
