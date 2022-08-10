6a1b7f17ced24aa35aa09bcd3bfde9fc82b1d518
ElmerPC <erey2790@gmail.com>
2022-08-09 22:38:54 -0500

    stripe not working atm

---
+++ client/build/precache-manifest.3f5375be6665d36941d75a8f1a1101db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1d3203e2a7835d21baf2a3f7856236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f7e0128add9be2b5f8",
+    "url": "/static/css/main.003fc7dc.chunk.css"
+  },
+  {
+    "revision": "4e58bf4710f60fa85b3c",
+    "url": "/static/js/2.6f5a3021.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6f5a3021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f7e0128add9be2b5f8",
+    "url": "/static/js/main.70748e0d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.70748e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
