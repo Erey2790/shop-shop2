225536312eee26f5b85eb6476091946d240e5bc5
ElmerPC <erey2790@gmail.com>
2022-12-19 14:05:18 -0600

    Updates

---
+++ precache-manifest.a19978f1f53bf42836f8b49e5035aadc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e96326b4ddd83330c54d19c7fa700cc",
+    "url": "/shop-shop2/index.html"
+  },
+  {
+    "revision": "38c3c683be96c560a2e4",
+    "url": "/shop-shop2/static/css/main.003fc7dc.chunk.css"
+  },
+  {
+    "revision": "cdb59017db06cf7868a1",
+    "url": "/shop-shop2/static/js/2.aeb11b67.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/shop-shop2/static/js/2.aeb11b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c3c683be96c560a2e4",
+    "url": "/shop-shop2/static/js/main.d21759ff.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/shop-shop2/static/js/main.d21759ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ef2e489eb606fcc9f0",
+    "url": "/shop-shop2/static/js/runtime-main.f1293e9a.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/shop-shop2/static/media/spinner.ab497855.gif"
+  }
+]);
